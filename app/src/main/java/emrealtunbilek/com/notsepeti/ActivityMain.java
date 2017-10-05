8d68b15fef27671fec7ca66092329a7ce5ce2544
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-05 19:35:04 +0300

    btn_renk_ekle.xml eklendi
    ilk selector not ekle butonunun rengi için oluşturuldu

--- app/src/main/java/emrealtunbilek/com/notsepeti/ActivityMain.java
+++ app/src/main/java/emrealtunbilek/com/notsepeti/ActivityMain.java
@@ -8,6 +8,6 @@ public class ActivityMain extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bos_liste);
+        setContentView(R.layout.activity_main);
     }
 }
