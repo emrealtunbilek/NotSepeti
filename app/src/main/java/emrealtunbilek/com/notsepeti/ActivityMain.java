af28a89d2ad79b3fb4f7d27a5e26708116e7f395
emre altunbilek <emrealtunbilek@gmail.com>
2017-10-05 19:25:39 +0300

    bos_liste.xml olusturuldu
    bos listedeki logo ve add butonu eklendi

--- app/src/main/java/emrealtunbilek/com/notsepeti/ActivityMain.java
+++ app/src/main/java/emrealtunbilek/com/notsepeti/ActivityMain.java
@@ -8,6 +8,6 @@ public class ActivityMain extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.bos_liste);
     }
 }
